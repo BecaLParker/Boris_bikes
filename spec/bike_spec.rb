3f2def29bf1af9c603b756b4dbd4d44a5f7dc779
Rebecca Parker <beca@Rebeccas-MacBook-Air.local>
2021-04-30 15:40:13 +0100

    Completes step 11 FINALLY

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,6 +1,8 @@
 require 'bike.rb'
 
 describe Bike do 
- #   it { is_expected.to respond_to :release_bike }
-    it { is_expected.to respond_to :working? }
+    #it { is_expected.to respond_to :working? }
+    it 'check if bike is working' do
+        expect(subject.working?).to eq true
+    end
 end
